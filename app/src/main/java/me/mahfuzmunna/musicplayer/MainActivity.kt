c66efa7e138b5e205caaa0531b34c859eb9af228
mahfuzmunna <mahfuzmunna17@proton.me>
2023-09-19 00:11:43 +0600

    splashScreen: added

--- app/src/main/java/me/mahfuzmunna/musicplayer/MainActivity.kt
+++ app/src/main/java/me/mahfuzmunna/musicplayer/MainActivity.kt
@@ -2,12 +2,14 @@ package me.mahfuzmunna.musicplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import me.mahfuzmunna.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
