fb37dfee5b158696d97275ac3f4b795c463a4ef2
mahfuzmunna <mahfuzmunna17@proton.me>
2023-09-19 10:19:59 +0600

    request_permission

--- app/src/main/java/me/mahfuzmunna/musicplayer/MainActivity.kt
+++ app/src/main/java/me/mahfuzmunna/musicplayer/MainActivity.kt
@@ -1,17 +1,49 @@
 package me.mahfuzmunna.musicplayer
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import me.mahfuzmunna.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val REQUEST_STORAGE_PERMISSION = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.initializePlayer.setOnClickListener {
+            if (isStoragePermission()) {
+                Toast.makeText(this, "Permitted", Toast.LENGTH_SHORT).show()
+            } else requestForPermission()
+
+        }
+    }
+
+
+    private fun requestForPermission() {
+
+        val permissions = arrayOf(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_STORAGE_PERMISSION)
+    }
+
+    private fun isStoragePermission(): Boolean {
+
+        return ((ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
+                )
     }
 }
